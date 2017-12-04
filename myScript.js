6ce846c48ee7010666ad138613e3418d6ce267c4
Bolaji Adewale <jamesadewale3@gmail.com>
2017-12-04 14:24:35 +0100

    I optimised the JS code by using JQuery UI

--- myScript.js
+++ myScript.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+function search() {
+	var liquidSearch = $("#liquidSearch");
+	var search = $("#search");
+	if (liquidSearch !=='') {search.html('You are searching for ')}
+	else {search.html('')};
+}
