2b1d83dfd207648a1469073cf97d73835eaca677
Bolaji Adewale <jamesadewale3@gmail.com>
2017-12-04 16:37:09 +0100

    I styled the document

--- myScript.js
+++ myScript.js
@@ -1,7 +1,14 @@
 // JavaScript Document
+	$("#filter").hide();
 function search() {
 	var liquidSearch = $("#liquidSearch");
 	var search = $("#search");
-	if (liquidSearch !=='') {search.html('You are searching for ')}
-	else {search.html('')};
+	if (liquidSearch !=='') {
+		search.html('You are searching for ');
+		$("#filter").show();
+		}
+	else {
+		search.html('');
+		$("#filter").hide();
+		};
 }
